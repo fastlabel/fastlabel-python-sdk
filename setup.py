b626078fb0cfaa170882f6b130d3cc66b5601f75
faycute <d.koizuka@gmail.com>
2021-07-19 19:51:14 +0900

    to 0.9.5

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", encoding="utf-8") as file:
 
 setuptools.setup(
     name="fastlabel",
-    version="0.9.4",
+    version="0.9.5",
     author="eisuke-ueta",
     author_email="eisuke.ueta@fastlabel.ai",
     description="The official Python SDK for FastLabel API, the Data Platform for AI",
