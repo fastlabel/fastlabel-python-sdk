c877427c317938ebd7a50be4ef8b8d9503d9d78e
ryoKaz <ryokazami@MacBook-Pro.local>
2021-12-06 10:23:07 +0900

    fix type decralation

--- fastlabel/__init__.py
+++ fastlabel/__init__.py
@@ -639,7 +639,7 @@ class Client:
         status: str = None,
         external_status: str = None,
         tags: list = [],
-        annotations: list[dict] = [],
+        annotations: list = [],
         **kwargs,
     ) -> str:
         """
