54eafcc8b3cf8965ed57ea375681d7c3fe6e330e
ryoKaz <ryokazami@MacBook-Pro.local>
2022-01-26 22:17:17 +0900

    update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", encoding="utf-8") as file:
 
 setuptools.setup(
     name="fastlabel",
-    version="0.11.12",
+    version="0.11.13",
     author="eisuke-ueta",
     author_email="eisuke.ueta@fastlabel.ai",
     description="The official Python SDK for FastLabel API, the Data Platform for AI",
