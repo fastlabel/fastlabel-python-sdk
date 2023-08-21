eb2875ba0a6e1b683a1a3ec73e6f22de18b46af9
Jiei-S <jiei7414@gmail.com>
2023-08-21 14:56:00 +0900

    feat: obsolete dataset version

--- examples/get_dataset_version_create_histories.py
+++
@@ -1,8 +0,0 @@
-from pprint import pprint
-
-import fastlabel
-
-client = fastlabel.Client()
-
-histories = client.get_dataset_version_create_histories(dataset_id="YOUR_DATASET_ID")
-pprint(histories)
