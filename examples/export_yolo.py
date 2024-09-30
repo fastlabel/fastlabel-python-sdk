352f08ad3bbdc4d424e70c0655fd0aa251f78acc
shuichi <shuichi.trfs+git@gmail.com>
2024-09-30 22:35:49 +0900

    exampleを追加

---
+++ examples/export_yolo.py
@@ -0,0 +1,7 @@
+import fastlabel
+client = fastlabel.Client()
+
+project_slug = "YOUR_PROJECT_SLUG"
+tasks = client.get_image_tasks(project=project_slug)
+
+client.export_yolo(project=project_slug, tasks=tasks, output_dir="./export_yolo/")
